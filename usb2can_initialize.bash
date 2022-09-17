fce95edf9d86b3cc36a25f8497daada6b368e415
Nathan Kau <nathankau@gmail.com>
2022-09-16 22:36:26 -0700

    add initialization for usb2can

---
+++ usb2can_initialize.bash
@@ -0,0 +1,6 @@
+sudo ip link set can0 down
+sudo ip link set can0 type can bitrate 1000000
+sudo ip link set can0 up
+sudo ip link set can1 down
+sudo ip link set can1 type can bitrate 1000000
+sudo ip link set can1 up
