0fbe17036cc1609be11b28d53b0f3bd92539cfcd
Nathan Kau <nathankau@gmail.com>
2022-10-10 13:01:33 +0900

    smaller timestep in launch files. install launch files

--- src/pupper_mujoco/launch/floating_base_launch.py
+++ src/pupper_mujoco/launch/floating_base_launch.py
@@ -12,13 +12,11 @@ def generate_launch_description():
             emulate_tty=True,
             parameters=[{
                 "publish_rate": 250.0,
-                "sim_step_rate": 250.0,
+                "sim_step_rate": 1000.0,
                 "sim_render_rate": 30.0,
                 "model_xml":
-                # "/home/nathan/pupperv3-testing/src/pupper_mujoco/src/urdf/pupper_v3_fixed_base.xml",
                 "/home/nathan/pupperv3-testing/src/pupper_mujoco/src/urdf/pupper_v3_floating_base.xml",
-                # "floating_base": False,
-                "floating_base":True,
-                "timestep": 0.004
+                "floating_base": True,
+                "timestep": 0.001
             }])
     ])
