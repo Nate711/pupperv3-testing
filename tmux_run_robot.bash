954f0fead19d517c52577a1254aaa5a6b9a11b23
Nathan Kau <nathankau@gmamil.com>
2023-07-31 00:40:29 +0900

    correct tmux script

--- tmux_run_robot.bash
+++ tmux_run_robot.bash
@@ -5,9 +5,9 @@ tmux new-session -d -s pupperv3
 tmux split-window -h
 tmux split-window -h
 
-tmux send-keys -t pupperv3:0 "source_ros; source_install" C-m
-tmux send-keys -t pupperv3:1 "source_ros; source_install" C-m
-tmux send-keys -t pupperv3:2 "source_ros; source_install" C-m
+tmux send-keys -t pupperv3:0.0 "source_ros; source_install; cd pupperv3-testing" C-m
+tmux send-keys -t pupperv3:0.1 "source_ros; source_install; cd StanfordQuarduped" C-m
+tmux send-keys -t pupperv3:0.2 "source_ros; source_install; ros2 run joy_linux joy_linux_node" C-m
 
 tmux select-layout -t pupperv3:0 even-horizontal
 
